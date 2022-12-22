5238e0a375577c2e9aef8db1593757774b147cf8
michaelfehr <michael_fehrgmx.de>
2022-12-23 00:50:31 +0100

    updated

--- app/src/main/java/de/androidcrypto/recyclerviewswipemenu/MainActivity.java
+++ app/src/main/java/de/androidcrypto/recyclerviewswipemenu/MainActivity.java
@@ -1,12 +1,10 @@
 package de.androidcrypto.recyclerviewswipemenu;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
